0d64d1e6bf46c9be8ad97997d66018343c5b25e1
Kisantini <s22a0069@siswa.umk.edu.my>
2025-10-31 02:15:15 +0800

    Change page title from 'Pencapaian Akademik' to 'Student Health'

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ st.set_page_config(
     page_title="Students Mental Health"
 )
 
-visualize = st.Page('studentsmental.py', title= 'Pencapaian Akademik', icon=":material/school:")
+visualize = st.Page('studentsmental.py', title= 'Student Health', icon=":material/school:")
 
 home = st.Page('home.py' , title = "Homepage",default=True, icon=":material/home:")
 
