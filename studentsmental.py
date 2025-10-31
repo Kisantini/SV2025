f8d962d7a227dc13fea4027452c3ad810bb29b66
Kisantini <s22a0069@siswa.umk.edu.my>
2025-10-31 14:42:50 +0800

    Add metrics for PLO 2 to PLO 5 in studentsmental.py

--- studentsmental.py
+++ studentsmental.py
@@ -68,6 +68,12 @@ st.markdown("This dashboard uses **Plotly** for dynamic and engaging visualizati
 if github_df.empty:
     st.stop()
 
+col1, col2, col3, col4 = st.columns(4)
+
+    col1.metric(label='PLO 2', value=f'3.5',help='PLO 2: Cognitive skill',border=True)
+    col2.metric(label='PLO 3', value=f'3.5',help='PLO 3: Digital skill',border=True)
+    col3.metric(label='PLO 4', value=f'4.0',help='PLO 4: Interpersonal skill',border=True)
+    col1.metric(label='PLO 5', value=f'4.3',help='PLO 2: Communication skill',border=True)
 # --- Visualization Functions ---
 
 def create_viz_block(title, chart, interpretation):
